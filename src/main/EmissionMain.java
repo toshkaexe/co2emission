af8c9dfd27c63d119b81d749639ffeadb56f6404
AntonZ <anton.zeltser@retarus.de>
2021-05-07 11:45:30 +0200

    test

--- src/main/EmissionMain.java
+++ src/main/EmissionMain.java
@@ -1,6 +1,10 @@
 package main;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.ParseException;
 import org.apache.http.client.ClientProtocolException;
@@ -39,6 +43,38 @@ public class EmissionMain {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
+		
+//		
+//		// Sending get request
+//        URL url = new URL("http://example-url");
+//        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//
+//        conn.setRequestProperty("Authorization","Bearer "+" Actual bearer token issued by provider.");
+//        //e.g. bearer token= eyJhbGciOiXXXzUxMiJ9.eyJzdWIiOiPyc2hhcm1hQHBsdW1zbGljZS5jb206OjE6OjkwIiwiZXhwIjoxNTM3MzQyNTIxLCJpYXQiOjE1MzY3Mzc3MjF9.O33zP2l_0eDNfcqSQz29jUGJC-_THYsXllrmkFnk85dNRbAw66dyEKBP5dVcFUuNTA8zhA83kk3Y41_qZYx43T
+//
+//        conn.setRequestProperty("Content-Type","application/json");
+//        conn.setRequestMethod("GET");
+//
+//
+//        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+//        String output;
+//
+//        StringBuffer response = new StringBuffer();
+//        while ((output = in.readLine()) != null) {
+//            response.append(output);
+//        }
+//
+//        in.close();
+//        // printing result from response
+//        System.out.println("Response:-" + response.toString());
+//		
+		
+		
+		
+		
+		
 
 	}
 
